e2f1c3218c2c638ff35e8a1690c7369235587edd
Oleksandr Kapara <a.kapara2006@gmail.com>
2023-03-11 10:37:08 +0200

    added try/catch

--- C_String_remake/Class.h
+++ C_String_remake/Class.h
@@ -2,194 +2,200 @@
 class String
 {
 private:
-	char* str;
-	int length;
+    char* str;
+    int length;
 
 
 public:
 
-	String()
-	{
-		str = nullptr;
-		length = 0;
-	}
+    String()
+    {
+        str = nullptr;
+        length = 0;
+    }
 
-	String(const String& other) : length(other.length)
-	{
-		if (other.str)
-		{
-			str = new char[length + 1];
-			std::strcpy(str, other.str);
-		}
-		else
-		{
-			str = nullptr;
-		}
-	}
+    String(const String& other) : length(other.length)
+    {
+        if (other.str)
+        {
+            str = new char[length + 1];
+            std::strcpy(str, other.str);
+        }
+        else
+        {
+            str = nullptr;
+        }
+    }
 
-	String(const char* str)
-	{
+    String(const char* str)
+    {
 
-		length = strlen(str);
-		this->str = new char[length + 1];
+        length = strlen(str);
+        this->str = new char[length + 1];
 
-		for (int i = 0; i < length; i++)
-		{
-			this->str[i] = str[i];
-		}
+        for (int i = 0; i < length; i++)
+        {
+            this->str[i] = str[i];
+        }
 
-		this->str[length] = '\0';
+        this->str[length] = '\0';
 
-	}
+    }
 
-	~String()
-	{
-		delete[] this->str;
-	}
+    ~String()
+    {
+        delete[] this->str;
+    }
 
-	String& operator =(const String& other)
-	{
-		if (this->str != nullptr)
-		{
-			delete[] str;
-		}
+    String& operator =(const String& other)
+    {
+        if (this->str != nullptr)
+        {
+            delete[] str;
+        }
 
-		length = strlen(other.str);
-		this->str = new char[length + 1];
+        length = strlen(other.str);
+        this->str = new char[length + 1];
 
-		for (int i = 0; i < length; i++)
-		{
-			this->str[i] = other.str[i];
-		}
+        for (int i = 0; i < length; i++)
+        {
+            this->str[i] = other.str[i];
+        }
 
-		this->str[length] = '\0';
+        this->str[length] = '\0';
 
-		return *this;
-	}
+        return *this;
+    }
 
-	String operator+(const String& other)
-	{
+    String operator+(const String& other)
+    {
 
-		String new_str;
+        String new_str;
 
-		int thisLength = strlen(this->str);
-		int otherLength = strlen(other.str);
+        int thisLength = strlen(this->str);
+        int otherLength = strlen(other.str);
 
-		new_str.length = thisLength + otherLength;
+        new_str.length = thisLength + otherLength;
 
-		new_str.str = new char[thisLength + otherLength + 1];
+        new_str.str = new char[thisLength + otherLength + 1];
 
-		int i = 0;
-		for (; i < thisLength; i++)
-		{
-			new_str.str[i] = this->str[i];
-		}
+        int i = 0;
+        for (; i < thisLength; i++)
+        {
+            new_str.str[i] = this->str[i];
+        }
 
-		for (int j = 0; j < otherLength; j++, i++)
-		{
-			new_str.str[i] = other.str[j];
-		}
+        for (int j = 0; j < otherLength; j++, i++)
+        {
+            new_str.str[i] = other.str[j];
+        }
 
-		new_str.str[thisLength + otherLength] = '\0';
+        new_str.str[thisLength + otherLength] = '\0';
 
-		return new_str;
-	}
+        return new_str;
+    }
 
-	void Print()
-	{
-		std::cout << str;
-	}
+    void Print()
+    {
+        std::cout << str;
+    }
 
-	int Length()
-	{
-		return length;
-	}
+    int Length()
+    {
+        return length;
+    }
 
-	bool operator ==(const String& other)
-	{
-		if (this->length != other.length)
-		{
-			return false;
-		}
+    bool operator ==(const String& other)
+    {
+        if (this->length != other.length)
+        {
+            return false;
+        }
 
-		for (int i = 0; i < this->length; i++)
-		{
-			if (this->str[i] != other.str[i])
-			{
-				return false;
-			}
-		}
-		return true;
-	}
+        for (int i = 0; i < this->length; i++)
+        {
+            if (this->str[i] != other.str[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	char& operator [](int index)
-	{
-		return this->str[index];
-	}
+    char& operator [](int index)
+    {
+        if (index < 0 || index >= length)
+        {
+            throw std::out_of_range("index out of bounds");
+        }
+        return this->str[index];
+    }
 
-	int& operator ()(char symb)
-	{
-		int len = strlen(this->str);
-		int wrong_ind = -1;
+    int& operator ()(char symb)
+    {
+        int len = strlen(this->str);
+        int wrong_ind = -1;
 
-		for (int i = 0; i <= len; i++)
-		{
-			if (this->str[i] == symb)
-			{
-				return i;
-			}
-		}
-		return wrong_ind;
-	}
+        for (int i = 0; i <= len; i++)
+        {
+            if (this->str[i] == symb)
+            {
+                return i;
+            }
+        }
+        return wrong_ind;
+    }
 
-	const int ToInt()
-	{
-		int len = strlen(this->str);
-		std::cout << len;
-		return len;
-	}
+    const int ToInt()
+    {
+        int len = strlen(this->str);
+        std::cout << len;
+        return len;
+    }
 
-	String(String&& other)
-	{
-		this->length = other.length;
-		this->str = other.str;
-		other.str = nullptr;
-	}
+    String(String&& other)
+    {
+        this->length = other.length;
+        this->str = other.str;
+        other.str = nullptr;
+    }
 
-	void DeleteChar(char c)
-	{
-		int index_c;
+    void DeleteChar(char c)
+    {
+        int index_c = -1;
+        int len = strlen(this->str);
 
-		char* newStr;
-		int len = strlen(this->str);
-		int newLen = len--;
+        for (int i = 0; i < len; i++)
+        {
+            if (this->str[i] == c)
+            {
+                index_c = i;
+                break;
+            }
+        }
 
-		newStr = new char[newLen];
+        if (index_c == -1)
+        {
+            Print();
+            return;
+        }
 
-		for (int i = 0; i < len; i++)
-		{
-			if (this->str[i] == c)
-			{
-				index_c = i;
-			}
-		}
+        char* newStr = new char[len];
+        int j = 0;
 
-		for (int i = 0; i < index_c; i++)
-		{
-			newStr[i] = this->str[i];
-		}
+        for (int i = 0; i < len; i++)
+        {
+            if (i != index_c)
+            {
+                newStr[j++] = this->str[i];
+            }
+        }
 
-		for (int i = index_c + 1; i < len; i++)
-		{
-			newStr[i - 1] = this->str[i];
-		}
+        newStr[j] = '\0';
+        delete[] this->str;
+        this->str = newStr;
+        this->length = strlen(newStr);
 
-		len -= 1;
-		this->str = newStr;
-
-		for (int i = 0; i < len; i++)
-		{
-			std::cout << this->str[i];
-		}
-	}
+        Print();
+    }
 };
