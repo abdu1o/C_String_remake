601bf4166f0fc324588fff54d902a31d668c1ed1
Oleksandr Kapara <a.kapara2006@gmail.com>
2023-03-11 10:38:10 +0200

    added try/catch

--- C_String_remake/C_String_remake.cpp
+++ C_String_remake/C_String_remake.cpp
@@ -1,28 +1,36 @@
 ﻿#include <iostream>
-#include "Class.h"
+#include <exception>
 #define _CRT_SECURE_NO_WARNINGS
+#include "Class.h"
 using namespace std;
 
 int main()
 {
-	String str1("Hello ");
-	String str2("World\n");
+    String str1("Hello ");
+    String str2("World\n");
 
-	String result;
-	result = str1 + str2;
-	result.Print();
+    String result;
+    result = str1 + str2;
+    result.Print();
 
-	String equal_result;
-	equal_result = str1 = str2;
-	equal_result.Print();
+    String equal_result;
+    equal_result = str1 = str2;
+    equal_result.Print();
 
-	char ind_result = str1[2];
-	cout << ind_result << endl;
+    try
+    {
+        char ind_result = str1[10];
+        cout << ind_result << endl;
+    }
+    catch (const out_of_range& ex)
+    {
+        cerr << "Wrong input: " << ex.what() << endl;
+    }
 
-	int char_result = str1('l');
-	cout << char_result << endl;
+    int char_result = str1('l');
+    cout << char_result << endl;
 
-	str1.ToInt();
+    str1.ToInt();
 
-	cout << endl;
+    cout << endl;
 }
